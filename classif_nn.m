092c82b010b30b30c006f79b4d16a69125f30f58
Mauricio Villegas <mauricio_ville@yahoo.com>
2008-07-11 09:57:19 +0000

    Added per class error option

--- classif_nn.m
+++ classif_nn.m
@@ -1,8 +1,8 @@
-function E = classif_nn(TR, TRid, TE, TEid)
+function E = classif_nn(TR, TRid, TE, TEid, varargin)
 %
 % CLASSIF_NN: Classify using Nearest Neighbor with the euclidean distance
 %
-% E = classif_nn(TR, TRid, TE, TEid)
+% E = classif_nn(TR, TRid, TE, TEid, ...)
 %
 %   Input:
 %     TR      - Training data matrix. Each column vector is a data point.
@@ -10,12 +10,15 @@ function E = classif_nn(TR, TRid, TE, TEid)
 %     TE      - Testing data matrix. Each column vector is a data point.
 %     TEid    - Testing data labels.
 %
+%   Input (optional):
+%     'perclass',(true|false)   - Compute error for each class (default=false)
+%
 %   Output:
 %     E       - Classification error
 %
-
+% Version: 1.01 -- Jul/2008
 %
-%   version 1.0 -- Apr/2008
+
 %
 %   Copyright (C) 2008 Mauricio Villegas (mvillegas AT iti.upv.es)
 %
@@ -36,26 +39,74 @@ function E = classif_nn(TR, TRid, TE, TEid)
 [D,N]=size(TR);
 M=size(TE,2);
 
-if size(TRid,1)~=N,
-  fprintf(2,'ldpp: error: size of TRid must be the same as the number of training points\n');
+perclass=false;
+
+n=1;
+argerr=false;
+while size(varargin,2)>0,
+  if ~ischar(varargin{n}) || size(varargin,2)<n+1,
+    argerr=true;
+  elseif strcmp(varargin{n},'perclass'),
+    eval([varargin{n},'=varargin{n+1};']);
+    if ~islogical(varargin{n+1}),
+      argerr=true;
+    else
+      n=n+2;
+    end
+  else
+    argerr=true;
+  end
+  if argerr || n>size(varargin,2),
+    break;
+  end
+end
+
+if argerr,
+  fprintf(2,'classif_nn: error: incorrect input argument (%d-%d)\n',n+5,n+6);
+elseif size(TRid,1)~=N,
+  fprintf(2,'classif_nn: error: size of TRid must be the same as the number of training points\n');
 elseif size(TEid,1)~=M,
-  fprintf(2,'ldpp: error: size of TEid must be the same as the number of testing points\n');
+  fprintf(2,'classif_nn: error: size of TEid must be the same as the number of testing points\n');
 elseif size(TE,1)~=D,
-  fprintf(2,'ldpp: error: dimensionality of train and test data points must be the same\n');
+  fprintf(2,'classif_nn: error: dimensionality of train and test data points must be the same\n');
 else
 
   dist=zeros(N,1);
 
-  E=0;
-  for m=1:M,
-    for n=1:N,
-      dist(n)=(TR(:,n)-TE(:,m))'*(TR(:,n)-TE(:,m));
+  if ~perclass,
+  
+    E=0;
+    for m=1:M,
+      for n=1:N,
+        dist(n)=(TR(:,n)-TE(:,m))'*(TR(:,n)-TE(:,m));
+      end
+      if min(dist(TRid==TEid(m)))>min(dist(TRid~=TEid(m))),
+        E=E+1;
+      end
     end
-    if min(dist(TRid==TEid(m)))>min(dist(TRid~=TEid(m))),
-      E=E+1;
-    end
-  end
 
-  E=E/M;
+    E=E/M;
+
+  else
+
+    Clabels=unique(TRid)';
+    C=max(size(Clabels));
+    E=zeros(C,1);
+    for m=1:M,
+      for n=1:N,
+        dist(n)=(TR(:,n)-TE(:,m))'*(TR(:,n)-TE(:,m));
+      end
+      if min(dist(TRid==TEid(m)))>min(dist(TRid~=TEid(m))),
+        c=find(Clabels==TEid(m));
+        E(c)=E(c)+1;
+      end
+    end
+    c=1;
+    for label=Clabels,
+      E(c)=E(c)/sum(TEid==label);
+      c=c+1;
+    end
+
+  end
 
 end
