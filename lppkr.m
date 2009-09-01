dee3b601ae4f24218fe57e9b73c9d2662e8530c3
Mauricio Villegas <mauricio_ville@yahoo.com>
2009-09-01 18:56:41 +0000

    working on regression

---
+++ lppkr.m
@@ -0,0 +1,427 @@
+function [bestB, bestP] = lppkr(X, XX, B0, P0, PP, varargin)
+%
+% LPPKR: Learning Discriminative Projections and Prototypes
+%
+% [B, P] = lppkr(X, XX, B0, P0, PP, ...)
+%
+%   Input:
+%     X       - Independent Data matrix. Each column vector is a data point.
+%     XX      - Dependent Data matrix.
+%     B0      - Initial projection base.
+%     P0      - Initial prototypes.
+%     PP      - Dependent Data for the prototypes.
+%
+%   Input (optional):
+%     'beta',BETA                - Sigmoid slope (defaul=1)
+%     'gamma',GAMMA              - Projection base learning rate (default=0.5)
+%     'eta',ETA                  - Prototypes learning rate (default=0.5)
+%     'epsilon',EPSILON          - Convergence criterium (default=1e-7)
+%     'minI',MINI                - Minimum number of iterations (default=100)
+%     'maxI',MAXI                - Maximum number of iterations (default=1000)
+%     'stats',STAT               - Statistics every STAT iterations (default=1000)
+%     'seed',SEED                - Random seed (default=system)
+%     'stochastic',(true|false)  - Stochastic gradient ascend (default=true)
+%     'orthoit',OIT              - Orthogonalize every OIT iterations (default=1)
+%     'orthonormal',(true|false) - Orthonormal projection base (default=true)
+%     'orthogonal',(true|false)  - Orthogonal projection base (default=false)
+%     'normalize',(true|false)   - Normalize training data (default=true)
+%     'whiten',(true|false)      - Whiten the training data (default=false)
+%     'protoweight',(true|false) - Weigth prototype update (default=false)
+%     'logfile',FID              - Output log file (default=stderr)
+%     'distance',('euclidean'|   - NN distance (default='euclidean')
+%                 'cosine')
+%
+%   Output:
+%     B   - Final learned projection base
+%     P   - Final learned prototypes
+%
+%
+% Version: 1.00 -- Sep/2009
+%
+
+%
+% Copyright (C) 2008 Mauricio Villegas (mvillegas AT iti.upv.es)
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
+%
+
+beta=1;
+gamma=0.5;
+eta=0.5;
+
+epsilon=1e-7;
+minI=100;
+maxI=1000;
+
+stochastic=false;
+seed=rand('seed');
+stats=1000;
+orthoit=100;
+
+orthonormal=true;
+orthogonal=false;
+normalize=true;
+whiten=false;
+linearnorm=false;
+distance='euclidean';
+
+logfile=2;
+
+n=1;
+argerr=false;
+while size(varargin,2)>0,
+  if ~ischar(varargin{n}) || size(varargin,2)<n+1,
+    argerr=true;
+  elseif strcmp(varargin{n},'beta') || ...
+         strcmp(varargin{n},'gamma') || ...
+         strcmp(varargin{n},'eta')  || ...
+         strcmp(varargin{n},'epsilon') || ...
+         strcmp(varargin{n},'minI') || ...
+         strcmp(varargin{n},'maxI') || ...
+         strcmp(varargin{n},'logfile') || ...
+         strcmp(varargin{n},'stats') || ...
+         strcmp(varargin{n},'orthoit') || ...
+         strcmp(varargin{n},'seed'),
+    eval([varargin{n},'=varargin{n+1};']);
+    if ~isnumeric(varargin{n+1}) || sum(varargin{n+1}<0),
+      argerr=true;
+    else
+      n=n+2;
+    end
+  elseif strcmp(varargin{n},'normalize') || ...
+         strcmp(varargin{n},'whiten') || ...
+         strcmp(varargin{n},'linearnorm') || ...
+         strcmp(varargin{n},'orthonormal') || ...
+         strcmp(varargin{n},'orthogonal') || ...
+         strcmp(varargin{n},'balance') || ...
+         strcmp(varargin{n},'protoweight') || ...
+         strcmp(varargin{n},'stochastic'),
+    eval([varargin{n},'=varargin{n+1};']);
+    if ~islogical(varargin{n+1}),
+      argerr=true;
+    else
+      if varargin{n+1}==true,
+        if strcmp(varargin{n},'normalize'),
+          whiten=false;
+          linearnorm=false;
+        elseif strcmp(varargin{n},'whiten'),
+          normalize=false;
+          linearnorm=false;
+        elseif strcmp(varargin{n},'linearnorm'),
+          normalize=false;
+          whiten=false;
+        elseif strcmp(varargin{n},'orthonormal'),
+          orthogonal=false;
+        elseif strcmp(varargin{n},'orthogonal'),
+          orthonormal=false;
+        end
+      end
+      n=n+2;
+    end
+  elseif strcmp(varargin{n},'distance'),
+    eval([varargin{n},'=varargin{n+1};']);
+    if ~ischar(varargin{n+1}),
+      argerr=true;
+    else
+      n=n+2;
+    end
+  else
+    argerr=true;
+  end
+  if argerr || n>size(varargin,2),
+    break;
+  end
+end
+
+N=size(X,2);
+D=size(X,1);
+DD=size(XX,1);
+R=size(B0,2);
+M=size(P0,2);
+
+if argerr,
+  fprintf(logfile,'lppkr: error: incorrect input argument (%d-%d)\n',varargin{n},varargin{n+1});
+elseif nargin-size(varargin,2)~=5,
+  fprintf(logfile,'lppkr: error: not enough input arguments\n');
+elseif size(B0,1)~=D,
+  fprintf(logfile,'lppkr: error: dimensionality of base and data must be the same\n');
+elseif size(P0,1)~=D,
+  fprintf(logfile,'lppkr: error: dimensionality of prototypes and data must be the same\n');
+elseif size(XX,2)~=N || size(PP,2)~=M,
+  fprintf(logfile,'lppkr: error: the number of vectors in the dependent and independent data must be the same\n');
+elseif size(PP,1)~=DD,
+  fprintf(logfile,'lppkr: error: the dimensionality of the dependent variables for the data and the prototypes must be the same\n');
+elseif ~(strcmp(distance,'euclidean') || strcmp(distance,'cosine')),
+  fprintf(logfile,'lppkr: error: invalid distance\n');
+else
+
+  if normalize || linearnorm,
+    xmu=mean(X,2);
+    xsd=std(X,1,2)*sqrt(D);
+    if linearnorm,
+      xsd=max(xsd)*ones(size(xsd));
+    end
+    X=(X-xmu(:,ones(N,1)))./xsd(:,ones(N,1));
+    P0=(P0-xmu(:,ones(M,1)))./xsd(:,ones(M,1));
+    B0=B0.*xsd(:,ones(R,1));
+    if sum(xsd==0)>0,
+      X(xsd==0,:)=[];
+      B0(xsd==0,:)=[];
+      P0(xsd==0,:)=[];
+      fprintf(logfile,'lppkr: warning: some dimensions have a standard deviation of zero\n');
+    end
+  elseif whiten,
+    [W,V]=pca(X);
+    W=W(:,V>1e-9);
+    V=V(V>1e-9);
+    W=W.*repmat((1./sqrt(V))',D,1);
+    W=(1/sqrt(R)).*W;
+    xmu=mean(X,2);
+    X=W'*(X-xmu(:,ones(N,1)));
+    P0=W'*(P0-xmu(:,ones(M,1)));
+    IW=pinv(W);
+    B0=IW*B0;
+  end
+
+  if orthonormal,
+    B0=orthonorm(B0);
+  elseif orthogonal,
+    B0=orthounit(B0);
+  end
+
+  if stochastic,
+    rand('seed',seed);
+  end
+
+  euclidean=true;
+  if strcmp(distance,'cosine'),
+    euclidean=false;
+  end
+
+  if euclidean,
+    gamma=2*gamma;
+    eta=2*eta;
+  end
+  NN=N*DD;
+  gamma=2*gamma*beta/NN;
+  eta=2*eta*beta/NN;
+
+  dist=zeros(M,N);
+  S=zeros(N,1);
+  mXX=zeros(size(XX));
+
+  B=B0;
+  P=P0;
+  bestB=B0;
+  bestP=P0;
+  bestI=0;
+  bestJ=1;
+  bestE=Inf;
+
+  J0=1;
+  I=0;
+
+  ppmu=mean(PP,2);
+  ppsd=std(PP,1,2);
+  XX=(XX-ppmu(:,ones(N,1)))./ppsd(:,ones(N,1));
+  PP=(PP-ppmu(:,ones(M,1)))./ppsd(:,ones(M,1));
+
+  ind=1:M;
+  ind=ind(ones(N,1),:);
+  ind=ind(:);
+
+  ind2=1:DD;
+  ind2=ind2(ones(N,1),:);
+  ind2=ind2(:);
+
+  fprintf(logfile,'lppkr: output: iteration | J | delta(J) | error\n');
+
+  tic;
+
+  if ~stochastic,
+
+    while 1,
+
+      rX=B'*X;
+      rP=B'*P;
+
+      if euclidean,
+
+        %tic
+        %for n=1:N,
+        %  dist(:,n)=exp(-sum(power(rX(:,n*ones(M,1))-rP,2),1));
+        %  S(n)=sum(dist(:,n));
+        %  mXX(:,n)=sum(dist(:,n*ones(DD,1))'.*PP,2)./S(n);
+        %end
+        %toc
+
+        %tic
+        dist=reshape(exp(-sum(power(repmat(rX,1,M)-rP(:,ind),2),1)),N,M);
+        S=sum(dist,2);
+        mXX=(reshape(sum(repmat(dist,DD,1).*PP(ind2,:),2),N,DD)./S(:,ones(DD,1)))';
+        %toc
+
+      %else % cosine
+
+        %rpsd=sqrt(sum(rP.*rP,1));
+        %rP=rP./rpsd(ones(R,1),:);
+        %for n=1:N,
+        %  rX(:,n)=rX(:,n)./sqrt(rX(:,n)'*rX(:,n));
+        %  dist(:,n)=exp(-(1-sum(rX(:,n*ones(M,1)).*rP,1)));
+        %  S(n)=sum(dist(:,n));
+        %  mXX(:,n)=sum(dist(:,n*ones(DD,1))'.*PP,2)./S(n);
+        %end
+
+        % faster implementation not working yet
+        %rpsd=sqrt(sum(rP.*rP,1));
+        %rP=rP./rpsd(ones(R,1),:);
+        %rxsd=sqrt(sum(rX.*rX,1));
+        %rX=rX./rxsd(ones(R,1),:);
+        %dist2=reshape(exp(-(1-sum(repmat(rX,1,M).*rP(:,ind),1))),N,M);
+        %S2=sum(dist2,2);
+        %mXX2=(reshape(sum(repmat(dist2,DD,1).*PP(ind2,:),2),N,DD)./S(:,ones(DD,1)))';
+
+      end
+
+      dXX=mXX-XX;
+      tanhXX=tanh(beta*sum(dXX.*dXX,1))';
+      J=sum(tanhXX)/NN;
+      E=sum(sum(dXX.*dXX,2).*ppsd.*ppsd)/NN;
+      %E=sum(sum(dXX.*dXX))/NN;
+
+      mark='';
+      if J<=bestJ,
+        bestB=B;
+        bestP=P;
+        bestI=I;
+        bestJ=J;
+        bestE=E;
+        mark=' *';
+      end
+
+      fprintf(logfile,'%d\t%.9f\t%.9f\t%f%s\n',I,J,J-J0,E,mark);
+
+      if I>=maxI,
+        fprintf(logfile,'lppkr: reached maximum number of iterations\n');
+        break;
+      end
+
+      if I>=minI,
+        if abs(J0-J)<epsilon,
+          fprintf(logfile,'lppkr: index has stabilized\n');
+          break;
+        end
+      end
+
+      J0=J;
+      I=I+1;
+
+      fX=zeros(R,N);
+      fP=zeros(R,M);
+
+      if euclidean,
+
+        tic
+        for n=1:N,
+          sumpp=sum(dist(n*ones(DD,1),:).*PP,2);
+          sump=dist(n,:).*sum(dXX(:,n*ones(M,1)).*(sumpp(:,ones(M,1))-S(n)*PP),1);
+          sump=sump.*((tanhXX(n)*tanhXX(n)-1)/(S(n)*S(n)));
+          frP=sump(ones(R,1),:).*(rX(:,n*ones(M,1))-rP);
+          fP=fP-frP;
+          fX(:,n)=fX(:,n)+sum(frP,2);
+        end
+        toc
+
+        fX2=zeros(R,N);
+        fP2=zeros(R,M);
+        tic
+        for n=1:N,
+          sump=dist(n,:).*sum(dXX(:,n*ones(M,1)).*(mXX(:,n*ones(M,1))-PP),1);
+          sump=sump.*((tanhXX(n)*tanhXX(n)-1)/S(n));
+          frP=sump(ones(R,1),:).*(rX(:,n*ones(M,1))-rP);
+          fP2=fP2-frP;
+          fX2(:,n)=fX2(:,n)+sum(frP,2);
+        end
+        toc
+
+        keyboard
+        tanhXX=(tanhXX.*tanhXX-1)./S;
+
+
+        P0=B*fP;
+        B0=X*fX'+P*fP';
+
+      end
+
+      B=B+gamma*B0; % there must be a mistake with a sign
+      P=P+eta*P0; % there must be a mistake with a sign
+
+      %if euclidean,
+
+        %rXs=(rX-rP(:,is)).*ds(:,ones(R,1))';
+        %rXd=(rX-rP(:,id)).*dd(:,ones(R,1))';
+        %for m=1:M,
+        %  rP0(:,m)=sum(rXd(:,id==m),2)-sum(rXs(:,is==m),2);
+        %end
+        %P0=B*rP0;
+        %Xs=X-P(:,is);
+        %Xd=X-P(:,id);
+        %B0=Xs*rXs'-Xd*rXd';
+
+      %  rXs=(rX-rP(:,is)).*ds(:,ones(R,1))';
+      %  rXd=(rX-rP(:,id)).*dd(:,ones(R,1))';
+      %  fX=rXs-rXd;
+      %  for m=1:M,
+      %    fP(:,m)=sum(rXd(:,id==m),2)-sum(rXs(:,is==m),2);
+      %  end
+      %  P0=B*fP;
+      %  B0=X*fX'+P*fP';
+      
+      %end
+
+      %B=B-gamma*B0;
+      %P=P-eta*P0;
+
+      if orthonormal,
+        B=orthonorm(B);
+      elseif orthogonal,
+        B=orthounit(B);
+      end
+
+    end
+
+  end
+
+  tm=toc;
+
+  if normalize || linearnorm,
+    bestP=bestP.*xsd(xsd~=0,ones(M,1))+xmu(xsd~=0,ones(M,1));
+    bestB=bestB./xsd(xsd~=0,ones(R,1));
+    if sum(xsd==0)>0,
+      P=bestP;
+      B=bestB;
+      bestP=zeros(D,M);
+      bestP(xsd~=0,:)=P;
+      bestB=zeros(D,R);
+      bestB(xsd~=0,:)=B;
+    end
+  elseif whiten,
+    bestP=IW'*bestP+xmu(:,ones(M,1));
+    bestB=W*bestB;
+  end
+
+  fprintf(logfile,'lppkr: average iteration time %f\n',tm/I);
+  fprintf(logfile,'lppkr: best iteration %d, J=%f, E=%f\n',bestI,bestJ,bestE);
+
+end
---
+++ regress_knn.m
@@ -0,0 +1,113 @@
+function XX = regress_knn(P, PP, K, X, varargin)
+%
+% REGRESS_KNN: K-NN regression
+%
+% XX = regress_knn(P, PP, K, X, ...)
+%
+%   Input:
+%     P        - Regression model independent variables.
+%     PP       - Regression model dependent variables.
+%     K        - Number of neighbors for the K-NN.
+%     X        - Data independent variables.
+%
+%   Input (optional):
+%     'distance',('euclidean'|  - NN distance (default='euclidean')
+%                 'cosine')
+%
+%   Output:
+%     XX       - Regression
+%
+%
+% Version: 1.00 -- Sep/2009
+%
+
+%
+% Copyright (C) 2008 Mauricio Villegas (mvillegas AT iti.upv.es)
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
+%
+
+[D,M]=size(P);
+DD=size(PP,1);
+N=size(X,2);
+
+distance='euclidean';
+
+n=1;
+argerr=false;
+while size(varargin,2)>0,
+  if ~ischar(varargin{n}) || size(varargin,2)<n+1,
+    argerr=true;
+  elseif strcmp(varargin{n},'distance'),
+    eval([varargin{n},'=varargin{n+1};']);
+    if ~ischar(varargin{n+1}),
+      argerr=true;
+    else
+      n=n+2;
+    end
+  else
+    argerr=true;
+  end
+  if argerr || n>size(varargin,2),
+    break;
+  end
+end
+
+if argerr,
+  fprintf(2,'regress_knn: error: incorrect input argument (%d-%d)\n',varargin{n},varargin{n+1});
+elseif nargin-size(varargin,2)~=4,
+  fprintf(2,'regress_knn: error: not enough input arguments\n');
+elseif size(PP,2)~=M,
+  fprintf(2,'regress_knn: error: the number of vectors in the dependent and independent data must be the same\n');
+elseif size(X,1)~=D,
+  fprintf(2,'regress_knn: error: dimensionality of the model and evaluation data must be the same\n');
+elseif ~(strcmp(distance,'euclidean') || strcmp(distance,'cosine')),
+  fprintf(2,'regress_knn: error: invalid distance\n');
+else
+
+  euclidean=true;
+  if strcmp(distance,'cosine'),
+    euclidean=false;
+  end
+
+  if euclidean,
+
+    ind=1:M;
+    ind=ind(ones(N,1),:);
+    ind=ind(:);
+
+    ind2=1:DD;
+    ind2=ind2(ones(N,1),:);
+    ind2=ind2(:);
+
+    dist=reshape(exp(-sum(power(repmat(X,1,M)-P(:,ind),2),1)),N,M);
+
+    if K==0,
+      S=sum(dist,2);
+      XX=(reshape(sum(repmat(dist,DD,1).*PP(ind2,:),2),N,DD)./S(:,ones(DD,1)))';
+    else
+      [dist,idx]=sort(dist',1);
+      dist=dist(1:K,:);
+      S=sum(dist,1)';
+      tPP=PP(ind2,:)';
+      ind3=M*[0:DD*N-1];
+      ind3=ind3(ones(M,1),:);
+      tPP=tPP(repmat(idx,1,DD)+ind3);
+      tPP=tPP(1:K,:);
+      XX=(reshape(sum(repmat(dist',DD,1).*tPP',2),N,DD)./S(:,ones(DD,1)))';
+    end
+
+  end
+
+end
