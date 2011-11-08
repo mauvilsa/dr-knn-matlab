878193276b802cfc3cf726ca81ebc519ae60988e
Mauricio Villegas <mauricio_ville@yahoo.com>
2011-11-08 10:08:42 +0000

    improved version and change of name for shoahd, other improvements

--- classify_nn.m
+++ classify_nn.m
@@ -1,9 +1,9 @@
-function [E, A, S, d] = classify_nn(P, Plabels, X, Xlabels, varargin)
+function [ E, A, S, d ] = classify_nn( P, Plabels, X, Xlabels, varargin )
 %
 % CLASSIFY_NN: Classify using Nearest Neighbor
 %
 % Usage:
-%   [E, A, S, dists] = classify_nn(P, Plabels, X, Xlabels, ...)
+%   [ E, A, S, dists ] = classify_nn( P, Plabels, X, Xlabels, ... )
 %
 % Input:
 %   P        - Prototypes data matrix. Each column vector is a data point.
@@ -15,6 +15,7 @@ function [E, A, S, d] = classify_nn(P, Plabels, X, Xlabels, varargin)
 %   'perclass',(true|false)   - Compute error/score for each class (default=false)
 %   'euclidean'               - Euclidean distance (default=true)
 %   'cosine'                  - Cosine distance (default=false)
+%   'hamming'                 - Hamming distance (default=false)
 %   'tangent'                 - Tangent distance (default=false)
 %   'rtangent'                - Ref. tangent distance (default=false)
 %   'otangent'                - Obs. tangent distance (default=false)
@@ -24,7 +25,7 @@ function [E, A, S, d] = classify_nn(P, Plabels, X, Xlabels, varargin)
 %
 % Output:
 %   E        - Classification error
-%   A        - Assigned Class
+%   A        - Assigned class
 %   S        - Classification score
 %   dists    - Pairwise distances
 %
@@ -32,7 +33,6 @@ function [E, A, S, d] = classify_nn(P, Plabels, X, Xlabels, varargin)
 % $Date$
 %
 
-%
 % Copyright (C) 2008-2010 Mauricio Villegas (mvillegas AT iti.upv.es)
 %
 % This program is free software: you can redistribute it and/or modify
@@ -47,287 +47,306 @@ function [E, A, S, d] = classify_nn(P, Plabels, X, Xlabels, varargin)
 %
 % You should have received a copy of the GNU General Public License
 % along with this program. If not, see <http://www.gnu.org/licenses/>.
-%
 
-if strncmp(P,'-v',2),
-  unix('echo "$Revision$* $Date$*" | sed "s/^:/classify_nn: revision/g; s/ : /[/g; s/ (.*)/]/g;"');
+fn = 'classify_nn:';
+minargs = 4;
+
+if ischar(P)
+  unix(['echo "$Revision$* $Date$*" | sed "s/^:/' fn ' revision/g; s/ : /[/g; s/ (.*)/]/g;"']);
   return;
 end
 
-fn='classify_nn:';
-minargs=4;
+E = [];
+A = [];
+S = [];
 
-E=[];
-A=[];
-S=[];
-
-[D,Np]=size(P);
-Nx=size(X,2);
-if size(Plabels,1)<size(Plabels,2),
-  Plabels=Plabels';
+[ D, Np ] = size(P);
+Nx = size(X,2);
+if size(Plabels,1)<size(Plabels,2)
+  Plabels = Plabels';
 end
-if size(Xlabels,1)<size(Xlabels,2),
-  Xlabels=Xlabels';
+if size(Xlabels,1)<size(Xlabels,2)
+  Xlabels = Xlabels';
 end
 
-perclass=false;
-torthonorm=false;
+perclass = false;
+torthonorm = false;
 
-dtype.euclidean=true;
-dtype.cosine=false;
-dtype.tangent=false;
-dtype.rtangent=false;
-dtype.otangent=false;
-dtype.atangent=false;
+dtype.euclidean = true;
+dtype.cosine = false;
+dtype.tangent = false;
+dtype.rtangent = false;
+dtype.otangent = false;
+dtype.atangent = false;
+dtype.hamming = false;
 
-logfile=2;
+logfile = 2;
 
-n=1;
-argerr=false;
-while size(varargin,2)>0,
-  if ~ischar(varargin{n}),
-    argerr=true;
+n = 1;
+argerr = false;
+while size(varargin,2)>0
+  if ~ischar(varargin{n})
+    argerr = true;
   elseif strcmp(varargin{n},'perclass') || ...
-         strcmp(varargin{n},'torthonorm'),
+         strcmp(varargin{n},'torthonorm')
     eval([varargin{n},'=varargin{n+1};']);
-    if ~islogical(varargin{n+1}),
-      argerr=true;
+    if ~islogical(varargin{n+1})
+      argerr = true;
     else
-      n=n+2;
+      n = n+2;
     end
   elseif strcmp(varargin{n},'euclidean') || ...
          strcmp(varargin{n},'tangent') || ...
          strcmp(varargin{n},'rtangent') || ...
          strcmp(varargin{n},'otangent') || ...
          strcmp(varargin{n},'atangent') || ...
-         strcmp(varargin{n},'cosine'),
-    dtype.euclidean=false;
-    dtype.cosine=false;
-    dtype.tangent=false;
-    dtype.rtangent=false;
-    dtype.otangent=false;
-    dtype.atangent=false;
+         strcmp(varargin{n},'hamming') || ...
+         strcmp(varargin{n},'cosine')
+    dtype.euclidean = false;
+    dtype.cosine = false;
+    dtype.tangent = false;
+    dtype.rtangent = false;
+    dtype.otangent = false;
+    dtype.atangent = false;
+    dtype.hamming = false;
     eval(['dtype.',varargin{n},'=true;']);
-    n=n+1;
+    n = n+1;
   elseif strcmp(varargin{n},'tangVp') || ...
-         strcmp(varargin{n},'tangVx'),
+         strcmp(varargin{n},'tangVx')
     eval([varargin{n},'=varargin{n+1};']);
-    if ~isnumeric(varargin{n+1}),
-      argerr=true;
+    if ~isnumeric(varargin{n+1})
+      argerr = true;
     else
-      n=n+2;
+      n = n+2;
     end
   else
-    argerr=true;
+    argerr = true;
   end
-  if argerr || n>size(varargin,2),
+  if argerr || n>size(varargin,2)
     break;
   end
 end
 
-if argerr,
+if argerr
   fprintf(logfile,'%s error: incorrect input argument %d (%s,%g)\n',fn,n+minargs,varargin{n},varargin{n+1});
   return;
-elseif nargin-size(varargin,2)~=minargs,
+elseif nargin-size(varargin,2)~=minargs
   fprintf(logfile,'%s error: not enough input arguments\n',fn);
   return;
-elseif size(X,1)~=D,
+elseif size(X,1)~=D
   fprintf(logfile,'%s error: dimensionality prototypes and data must be the same\n',fn);
   return;
 elseif size(Plabels,1)~=Np || size(Plabels,2)~=1 || ...
-      (sum(size(Xlabels))~=0&&(size(Xlabels,1)~=Nx || size(Xlabels,2)~=1)),
+      ( sum(size(Xlabels))~=0 && ( size(Xlabels,1)~=Nx || size(Xlabels,2)~=1 ) )
   fprintf(logfile,'%s error: labels must have the same size as the number of data points\n',fn);
   return;
-elseif ~exist('tangVp','var') && (dtype.tangent || dtype.atangent || dtype.rtangent),
+elseif ~exist('tangVp','var') && ( dtype.tangent || dtype.atangent || dtype.rtangent )
   fprintf(logfile,'%s error: tangents of prototypes should be given\n',fn);
   return;
-elseif ~exist('tangVx','var') && (dtype.tangent || dtype.atangent || dtype.otangent),
+elseif ~exist('tangVx','var') && ( dtype.tangent || dtype.atangent || dtype.otangent )
   fprintf(logfile,'%s error: tangents of testing data should be given\n',fn);
   return;
-elseif (exist('tangVp','var') && mod(size(tangVp,2),Np)~=0) || ...
-       (exist('tangVx','var') && mod(size(tangVx,2),Nx)~=0),
+elseif ( exist('tangVp','var') && mod(size(tangVp,2),Np)~=0 ) || ...
+       ( exist('tangVx','var') && mod(size(tangVx,2),Nx)~=0 )
   fprintf(logfile,'%s error: number of tangents should be a multiple of the number of samples\n',fn);
   return;
 end
 
-onesNp=ones(Np,1);
-onesNx=ones(Nx,1);
-onesD=ones(D,1);
+onesNp = ones(Np,1);
+onesNx = ones(Nx,1);
+onesD = ones(D,1);
 
-if exist('tangVp','var') && (dtype.rtangent || dtype.atangent || dtype.tangent),
-  Lp=size(tangVp,2)/Np;
-  if torthonorm || sum(sum(eye(Lp)-round(1000*tangVp(:,1:Lp)'*tangVp(:,1:Lp))./1000))~=0,
-    if ~torthonorm,
+if exist('tangVp','var') && ( dtype.rtangent || dtype.atangent || dtype.tangent )
+  Lp = size(tangVp,2)/Np;
+  if torthonorm || sum(sum(eye(Lp)-round(1000*tangVp(:,1:Lp)'*tangVp(:,1:Lp))./1000))~=0
+    if ~torthonorm
       fprintf(logfile,'%s warning: tangVp not orthonormal, orthonormalizing ...\n',fn);
     end
-    for nlp=1:Lp:size(tangVp,2),
-      %tangVp(:,nlp:nlp+Lp-1)=orthonorm(tangVp(:,nlp:nlp+Lp-1));
-      [orthoVp,dummy]=qr(tangVp(:,nlp:nlp+Lp-1),0);
-      tangVp(:,nlp:nlp+Lp-1)=orthoVp;
+    for nlp=1:Lp:size(tangVp,2)
+      [ orthoVp, dummy ] = qr(tangVp(:,nlp:nlp+Lp-1),0);
+      tangVp(:,nlp:nlp+Lp-1) = orthoVp;
     end
   end
 end
-if exist('tangVx','var') && (dtype.otangent || dtype.atangent || dtype.tangent),
-  Lx=size(tangVx,2)/Nx;
-  if torthonorm || sum(sum(eye(Lx)-round(1000*tangVx(:,1:Lx)'*tangVx(:,1:Lx))./1000))~=0,
-    if ~torthonorm,
+if exist('tangVx','var') && ( dtype.otangent || dtype.atangent || dtype.tangent )
+  Lx = size(tangVx,2)/Nx;
+  if torthonorm || sum(sum(eye(Lx)-round(1000*tangVx(:,1:Lx)'*tangVx(:,1:Lx))./1000))~=0
+    if ~torthonorm
       fprintf(logfile,'%s warning: tangVx not orthonormal, orthonormalizing ...\n',fn);
     end
-    for nlx=1:Lx:size(tangVx,2),
-      %tangVx(:,nlx:nlx+Lx-1)=orthonorm(tangVx(:,nlx:nlx+Lx-1));
-      [orthoVx,dummy]=qr(tangVx(:,nlx:nlx+Lx-1),0);
-      tangVx(:,nlx:nlx+Lx-1)=orthoVx;
+    for nlx=1:Lx:size(tangVx,2)
+      [ orthoVx, dummy ] = qr(tangVx(:,nlx:nlx+Lx-1),0);
+      tangVx(:,nlx:nlx+Lx-1) = orthoVx;
     end
   end
 end
 
 % euclidean distance
-if dtype.euclidean,
-  x2=sum((X.^2),1)';
-  p2=sum((P.^2),1);
-  d=X'*P;
-  d=x2(:,onesNp)+p2(onesNx,:)-d-d;
+if dtype.euclidean
+  x2 = sum((X.^2),1)';
+  p2 = sum((P.^2),1);
+  d = X'*P;
+  d = x2(:,onesNp)+p2(onesNx,:)-d-d;
 % cosine distance
-elseif dtype.cosine,
-  psd=sqrt(sum(P.*P,1));
-  P=P./psd(onesD,:);
-  xsd=sqrt(sum(X.*X,1));
-  X=X./xsd(onesD,:);
-  d=1-X'*P;
+elseif dtype.cosine
+  psd = sqrt(sum(P.*P,1));
+  P = P./psd(onesD,:);
+  xsd = sqrt(sum(X.*X,1));
+  X = X./xsd(onesD,:);
+  d = 1-X'*P;
 % reference single sided tangent distance
-elseif dtype.rtangent,
-  d=zeros(Nx,Np);
-  Lp=size(tangVp,2)/Np;
-  nlp=1;
-  for np=1:Np,
-    dXP=X-P(:,np(onesNx));
-    VdXP=tangVp(:,nlp:nlp+Lp-1)'*dXP;
-    d(:,np)=(sum(dXP.*dXP,1)-sum(VdXP.*VdXP,1))';
-    nlp=nlp+Lp;
+elseif dtype.rtangent
+  d = zeros(Nx,Np);
+  Lp = size(tangVp,2)/Np;
+  nlp = 1;
+  for np=1:Np
+    dXP = X-P(:,np(onesNx));
+    VdXP = tangVp(:,nlp:nlp+Lp-1)'*dXP;
+    d(:,np) = (sum(dXP.*dXP,1)-sum(VdXP.*VdXP,1))';
+    nlp = nlp+Lp;
   end
 % observation single sided tangent distance
-elseif dtype.otangent,
-  d=zeros(Nx,Np);
-  Lx=size(tangVx,2)/Nx;
-  nlx=1;
-  for nx=1:Nx,
-    dXP=X(:,nx(onesNp))-P;
-    VdXP=tangVx(:,nlx:nlx+Lx-1)'*dXP;
-    d(nx,:)=sum(dXP.*dXP,1)-sum(VdXP.*VdXP,1);
-    nlx=nlx+Lx;
+elseif dtype.otangent
+  d = zeros(Nx,Np);
+  Lx = size(tangVx,2)/Nx;
+  nlx = 1;
+  for nx=1:Nx
+    dXP = X(:,nx(onesNp))-P;
+    VdXP = tangVx(:,nlx:nlx+Lx-1)'*dXP;
+    d(nx,:) = sum(dXP.*dXP,1)-sum(VdXP.*VdXP,1);
+    nlx = nlx+Lx;
   end
 % average single sided tangent distance
-elseif dtype.atangent,
-  d=zeros(Nx,Np);
-  Lp=size(tangVp,2)/Np;
-  nlp=1;
-  for np=1:Np,
-    dXP=X-P(:,np(onesNx));
-    VdXP=tangVp(:,nlp:nlp+Lp-1)'*dXP;
-    d(:,np)=(sum(dXP.*dXP,1)-0.5*sum(VdXP.*VdXP,1))';
-    nlp=nlp+Lp;
+elseif dtype.atangent
+  d = zeros(Nx,Np);
+  Lp = size(tangVp,2)/Np;
+  nlp = 1;
+  for np=1:Np
+    dXP = X-P(:,np(onesNx));
+    VdXP = tangVp(:,nlp:nlp+Lp-1)'*dXP;
+    d(:,np) = (sum(dXP.*dXP,1)-0.5*sum(VdXP.*VdXP,1))';
+    nlp = nlp+Lp;
   end
-  Lx=size(tangVx,2)/Nx;
-  nlx=1;
-  for nx=1:Nx,
-    dXP=X(:,nx(onesNp))-P;
-    VdXP=tangVx(:,nlx:nlx+Lx-1)'*dXP;
-    d(nx,:)=d(nx,:)-0.5*sum(VdXP.*VdXP,1);
-    nlx=nlx+Lx;
+  Lx = size(tangVx,2)/Nx;
+  nlx = 1;
+  for nx=1:Nx
+    dXP = X(:,nx(onesNp))-P;
+    VdXP = tangVx(:,nlx:nlx+Lx-1)'*dXP;
+    d(nx,:) = d(nx,:)-0.5*sum(VdXP.*VdXP,1);
+    nlx = nlx+Lx;
   end
 % tangent distance
-elseif dtype.tangent,
-  d=zeros(Nx,Np);
-  Lp=size(tangVp,2)/Np;
-  Lx=size(tangVx,2)/Nx;
-  tangVpp=zeros(Lp,Lp*Np);
-  itangVpp=zeros(Lp,Lp*Np);
-  tangVxx=zeros(Lx,Lx*Nx);
-  itangVxx=zeros(Lx,Lx*Nx);
-  nlp=1;
-  for np=1:Np,
-    sel=nlp:nlp+Lp-1;
-    Vp=tangVp(:,sel);
-    tangVpp(:,sel)=Vp'*Vp;
-    itangVpp(:,sel)=inv(tangVpp(:,sel));
-    nlp=nlp+Lp;
+elseif dtype.tangent
+  d = zeros(Nx,Np);
+  Lp = size(tangVp,2)/Np;
+  Lx = size(tangVx,2)/Nx;
+  tangVpp = zeros(Lp,Lp*Np);
+  itangVpp = zeros(Lp,Lp*Np);
+  tangVxx = zeros(Lx,Lx*Nx);
+  itangVxx = zeros(Lx,Lx*Nx);
+  nlp = 1;
+  for np=1:Np
+    sel = nlp:nlp+Lp-1;
+    Vp = tangVp(:,sel);
+    tangVpp(:,sel) = Vp'*Vp;
+    itangVpp(:,sel) = inv(tangVpp(:,sel));
+    nlp = nlp+Lp;
   end
-  nlx=1;
-  for nx=1:Nx,
-    sel=nlx:nlx+Lx-1;
-    Vx=tangVx(:,sel);
-    tangVxx(:,sel)=Vx'*Vx;
-    itangVxx(:,sel)=inv(tangVxx(:,sel));
-    nlx=nlx+Lx;
+  nlx = 1;
+  for nx=1:Nx
+    sel = nlx:nlx+Lx-1;
+    Vx = tangVx(:,sel);
+    tangVxx(:,sel) = Vx'*Vx;
+    itangVxx(:,sel) = inv(tangVxx(:,sel));
+    nlx = nlx+Lx;
   end
-  nlx=1;
-  for nx=1:Nx,
-    sel=nlx:nlx+Lx-1;
-    nlx=nlx+Lx;
-    Vx=tangVx(:,sel);
-    Vxx=tangVxx(:,sel);
-    iVxx=itangVxx(:,sel);
-    x=X(:,nx);
-    nlp=1;
-    for np=1:Np,
-      sel=nlp:nlp+Lp-1;
-      nlp=nlp+Lp;
-      Vp=tangVp(:,sel);
-      Vpp=tangVpp(:,sel);
-      iVpp=itangVpp(:,sel);
-      p=P(:,np);
-      Vpx=Vp'*Vx;
-      Alp=(Vpx*iVxx*Vx'-Vp')*(x-p);
-      Arp=Vpx*iVxx*Vpx'-Vpp;
-      Alx=(Vpx'*iVpp*Vp'-Vx')*(x-p);
-      Arx=Vxx-Vpx'*iVpp*Vpx;
-      ap=Arp\Alp;
-      ax=Arx\Alx;
-      xx=x+Vx*ax;
-      pp=p+Vp*ap;
-      d(nx,np)=(xx-pp)'*(xx-pp);
+  nlx = 1;
+  for nx=1:Nx
+    sel = nlx:nlx+Lx-1;
+    nlx = nlx+Lx;
+    Vx = tangVx(:,sel);
+    Vxx = tangVxx(:,sel);
+    iVxx = itangVxx(:,sel);
+    x = X(:,nx);
+    nlp = 1;
+    for np=1:Np
+      sel = nlp:nlp+Lp-1;
+      nlp = nlp+Lp;
+      Vp = tangVp(:,sel);
+      Vpp = tangVpp(:,sel);
+      iVpp = itangVpp(:,sel);
+      p = P(:,np);
+      Vpx = Vp'*Vx;
+      Alp = (Vpx*iVxx*Vx'-Vp')*(x-p);
+      Arp = Vpx*iVxx*Vpx'-Vpp;
+      Alx = (Vpx'*iVpp*Vp'-Vx')*(x-p);
+      Arx = Vxx-Vpx'*iVpp*Vpx;
+      ap = Arp\Alp;
+      ax = Arx\Alx;
+      xx = x+Vx*ax;
+      pp = p+Vp*ap;
+      d(nx,np) = (xx-pp)'*(xx-pp);
     end
   end
-end
-
-Clabels=unique(Plabels)';
-Cp=max(size(Clabels));
-
-dist=zeros(Nx,Cp);
-c=1;
-for label=Clabels,
-  csel=Plabels==label;
-  dist(:,c)=min(d(:,csel),[],2);
-  c=c+1;
-end
-
-[idist,A]=min(dist,[],2);
-
-if nargout>2,
-  idist=dist;
-  idist(idist==0)=realmin;
-  idist=1./idist;
-  if perclass,
-    S=idist./repmat(sum(idist,2),1,Cp);
-  else
-    S=idist(Nx*(A-1)+[1:Nx]')./sum(idist,2);
+% hamming distance
+elseif dtype.hamming
+  lup = uint16([...
+    0 1 1 2 1 2 2 3 1 2 2 3 2 3 3 4 1 2 2 3 2 3 ...
+    3 4 2 3 3 4 3 4 4 5 1 2 2 3 2 3 3 4 2 3 3 4 ...
+    3 4 4 5 2 3 3 4 3 4 4 5 3 4 4 5 4 5 5 6 1 2 ...
+    2 3 2 3 3 4 2 3 3 4 3 4 4 5 2 3 3 4 3 4 4 5 ...
+    3 4 4 5 4 5 5 6 2 3 3 4 3 4 4 5 3 4 4 5 4 5 ...
+    5 6 3 4 4 5 4 5 5 6 4 5 5 6 5 6 6 7 1 2 2 3 ...
+    2 3 3 4 2 3 3 4 3 4 4 5 2 3 3 4 3 4 4 5 3 4 ...
+    4 5 4 5 5 6 2 3 3 4 3 4 4 5 3 4 4 5 4 5 5 6 ...
+    3 4 4 5 4 5 5 6 4 5 5 6 5 6 6 7 2 3 3 4 3 4 ...
+    4 5 3 4 4 5 4 5 5 6 3 4 4 5 4 5 5 6 4 5 5 6 ...
+    5 6 6 7 3 4 4 5 4 5 5 6 4 5 5 6 5 6 6 7 4 5 ...
+    5 6 5 6 6 7 5 6 6 7 6 7 7 8]);
+  d = zeros(Nx,Np);
+  for nx=1:Nx
+    d(nx,:) = sum(lup(1+uint16(bitxor(P,X(:,nx(onesNp))))),1);
   end
 end
 
-if sum(size(Xlabels))~=0,
-  if perclass,
-    E=zeros(Cp,1);
-    c=1;
-    for label=Clabels,
-      sel=Xlabels==label;
-      E(c)=sum(Clabels(A(sel))'~=label)/sum(sel);
-      c=c+1;
+Clabels = unique(Plabels)';
+Cp = max(size(Clabels));
+
+dist = zeros(Nx,Cp);
+c = 1;
+for label=Clabels
+  csel = Plabels==label;
+  dist(:,c) = min(d(:,csel),[],2);
+  c = c+1;
+end
+
+[ idist, A ] = min(dist,[],2);
+
+if nargout>2
+  idist = dist;
+  idist(idist==0) = realmin;
+  idist = 1./idist;
+  if perclass
+    S = idist./repmat(sum(idist,2),1,Cp);
+  else
+    S = idist(Nx*(A-1)+[1:Nx]')./sum(idist,2);
+  end
+end
+
+if sum(size(Xlabels))~=0
+  if perclass
+    E = zeros(Cp,1);
+    c = 1;
+    for label=Clabels
+      sel = Xlabels==label;
+      E(c) = sum(Clabels(A(sel))'~=label)/sum(sel);
+      c = c+1;
     end
   else
-    E=sum(Clabels(A)'~=Xlabels)/Nx;
+    E = sum(Clabels(A)'~=Xlabels)/Nx;
   end
 end
 
-if nargout>1,
-  A=Clabels(A)';
+if nargout>1
+  A = Clabels(A)';
 end
